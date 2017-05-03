69ba0ddce4c95aa29037b14f2c293aceff3cc9fe
nikhilsangwan007 <nikhil.sangwan95@gmail.com>
2017-05-03 10:43:57 +0530

    second commit

---
+++ data.py
@@ -0,0 +1,39 @@
+def Articles():
+	articles = [
+		{
+			'id' : 1,
+			'title' : 'One Piece',
+			'body' : 'pirates anime',
+			'author' : 'Oda',
+			'releaseYear' : '1999'
+		},
+		{
+			'id' : 2,
+			'title' : 'Naruto',
+			'body' : 'ninja anime',
+			'author' : 'john doe',
+			'releaseYear' : '2003'
+		},
+		{
+			'id' : 3,
+			'title' : 'Dragon ball z',
+			'body' : 'sayians anime',
+			'author' : 'akira toriyama',
+			'releaseYear' : '1985'
+		},
+		{
+			'id' : 4,
+			'title' : 'Bleach',
+			'body' : 'reapers anime',
+			'author' : 'john doe',
+			'releaseYear' : '2000'
+		},
+		{
+			'id' : 5,
+			'title' : 'hunterxhunter',
+			'body' : 'strong anime',
+			'author' : 'nikhil sangwan',
+			'releaseYear' : '1965'
+		}
+	]
+	return articles
